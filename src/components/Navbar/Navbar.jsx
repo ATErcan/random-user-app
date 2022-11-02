7824e664095c51f83c0c30987f797f877c2c44e6
Talha <atercan0658@gmail.com>
2022-11-02 15:51:25 +0100

    navbar created

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="author">
+        <h2 className="author-name">Talha Ercan</h2>
+        <p className="author-role">Full-Stack Developer</p>
+      </div>
+      <h1 className="project-title">Random User App</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
