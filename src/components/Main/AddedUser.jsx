56f95457d0e7c319193c9a5333f537739912ab26
Talha <atercan0658@gmail.com>
2022-11-03 22:41:56 +0100

    addUser func added

---
+++ src/components/Main/AddedUser.jsx
@@ -0,0 +1,20 @@
+const AddedUser = () => {
+  return (
+    <div className="user-list">
+      <div className="firstname-title user-title">
+        <p>Firstname</p>
+      </div>
+      <div className="email-title user-title">
+        <p>Email</p>
+      </div>
+      <div className="phone-title user-title">
+        <p>Phone</p>
+      </div>
+      <div className="age-title user-title">
+        <p>Age</p>
+      </div>
+    </div>
+  );
+};
+
+export default AddedUser;
--- src/components/Main/Person.jsx
+++ src/components/Main/Person.jsx
@@ -9,6 +9,7 @@ import gum from "../../assets/growing-up-man.svg";
 import map from "../../assets/map.svg";
 import phone from "../../assets/phone.svg";
 import padlock from "../../assets/padlock.svg";
+import AddedUser from "./AddedUser";
 
 const Person = () => {
   const [randomUser, setRandomUser] = useState([]);
@@ -17,18 +18,12 @@ const Person = () => {
     title: "",
     detail: "",
   });
+  const [userList, setuserList] = useState([]);
 
   useEffect(() => {
     const url = "https://randomuser.me/api/";
     axios(url).then((data) => setRandomUser(data.data.results));
   }, [newUser]);
-
-  useEffect(() => {
-    /*     setInfo({
-      title: "My name is",
-      detail: `${randomUser[0]?.name.first} ${randomUser[0]?.name.last}`,
-    }); */
-  }, []);
 
   const getNewUser = () => {
     setNewUser((prevUser) => !prevUser);
@@ -72,72 +67,94 @@ const Person = () => {
     }
   };
 
+  const addUser = () => {
+    const isSame = (user) =>
+      user.firstName ===
+      `${randomUser[0].name.first} ${randomUser[0].name.last}`;
+    if (!userList.some(isSame)) {
+      setuserList((prevList) => [
+        ...prevList,
+        {
+          firstName: `${randomUser[0].name.first} ${randomUser[0].name.last}`,
+          email: randomUser[0].email,
+          phone: randomUser[0].phone,
+          age: randomUser[0].dob.age,
+        },
+      ]);
+    }
+  };
+
   return (
-    <div className="person">
-      <img
-        src={randomUser[0]?.picture.large}
-        alt="person"
-        className="person-img"
-      />
-      <div className="person-info">
-        <p className="my-info">{!info.title ? "My name is" : info.title}</p>
-        <h3 className="info">
-          {!info.title
-            ? `${randomUser[0]?.name.first} ${randomUser[0]?.name.last}`
-            : info.detail}
-        </h3>
+    <>
+      <div className="person">
+        <img
+          src={randomUser[0]?.picture.large}
+          alt="person"
+          className="person-img"
+        />
+        <div className="person-info">
+          <p className="my-info">{info.title || "My name is"}</p>
+          <h3 className="info">
+            {!info.title
+              ? `${randomUser[0]?.name.first} ${randomUser[0]?.name.last}`
+              : info.detail}
+          </h3>
+        </div>
+        <div className="icon-container">
+          <img
+            src={randomUser[0]?.gender === "male" ? man : woman}
+            alt="gender"
+            className="icon"
+            onMouseOver={showInfo}
+            id="gender"
+          />
+          <img
+            src={mail}
+            alt="email"
+            className="icon"
+            onMouseOver={showInfo}
+            id="mail"
+          />
+          <img
+            src={randomUser[0]?.gender === "male" ? gum : guw}
+            alt="age"
+            className="icon"
+            onMouseOver={showInfo}
+            id="age"
+          />
+          <img
+            src={map}
+            alt="address"
+            className="icon"
+            onMouseOver={showInfo}
+            id="address"
+          />
+          <img
+            src={phone}
+            alt="phone"
+            className="icon"
+            onMouseOver={showInfo}
+            id="phone"
+          />
+          <img
+            src={padlock}
+            alt="password"
+            className="icon"
+            onMouseOver={showInfo}
+            id="password"
+          />
+        </div>
+        <div className="btn-container">
+          <button className="new-user btn" onClick={getNewUser}>
+            NEW USER
+          </button>
+          <button className="add-user btn" onClick={addUser}>
+            ADD USER
+          </button>
+        </div>
       </div>
-      <div className="icon-container">
-        <img
-          src={randomUser[0]?.gender === "male" ? man : woman}
-          alt="gender"
-          className="icon"
-          onMouseOver={showInfo}
-          id="gender"
-        />
-        <img
-          src={mail}
-          alt="email"
-          className="icon"
-          onMouseOver={showInfo}
-          id="mail"
-        />
-        <img
-          src={randomUser[0]?.gender === "male" ? gum : guw}
-          alt="age"
-          className="icon"
-          onMouseOver={showInfo}
-          id="age"
-        />
-        <img
-          src={map}
-          alt="address"
-          className="icon"
-          onMouseOver={showInfo}
-          id="address"
-        />
-        <img
-          src={phone}
-          alt="phone"
-          className="icon"
-          onMouseOver={showInfo}
-          id="phone"
-        />
-        <img
-          src={padlock}
-          alt="password"
-          className="icon"
-          onMouseOver={showInfo}
-          id="password"
-        />
-      </div>
-      <div className="btn-container">
-        <button className="new-user btn" onClick={getNewUser}>
-          NEW USER
-        </button>
-        <button className="add-user btn">ADD USER</button>
-      </div>
-    </div>
+      <AddedUser />
+    </>
   );
 };
 
